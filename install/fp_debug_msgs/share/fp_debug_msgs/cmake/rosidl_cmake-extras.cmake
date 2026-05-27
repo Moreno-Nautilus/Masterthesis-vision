aedf38a9acd661590eb38c1b501db1911a7b8396
Moreno-Nautilus <moreno.waelchli@aris-space.ch>
2026-05-27 15:10:51 +0200

    Remove local generated files from repository

--- install/fp_debug_msgs/share/fp_debug_msgs/cmake/rosidl_cmake-extras.cmake
+++
@@ -1,4 +0,0 @@
-# generated from rosidl_cmake/cmake/rosidl_cmake-extras.cmake.in
-
-set(fp_debug_msgs_IDL_FILES "msg/DebugMaskCrop.idl;msg/DebugCandidate.idl;msg/DebugPoseItem.idl;msg/DebugFrame.idl")
-set(fp_debug_msgs_INTERFACE_FILES "msg/DebugMaskCrop.msg;msg/DebugCandidate.msg;msg/DebugPoseItem.msg;msg/DebugFrame.msg")
